b3c6e23b620b46bcf6b5359d878b98de9f7701bd
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-05 11:38:02 -0500

    dynamic binding

--- CSharp.Course.Work/Version4.cs
+++ CSharp.Course.Work/Version4.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,24 @@ namespace CSharp.Course.Work
     [TestFixture]
     public class Version4
     {
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/dotnet/articles/csharp/programming-guide/types/walkthrough-creating-and-using-dynamic-objects
+        /// </summary>
         [Test]
         public void DynamicBinding()
         {
-            Assert.Ignore();
+            // Assert.Ignore();
+
+            dynamic obj = new ExpandoObject();
+            obj.StringProperty = "This is a string";
+            obj.IntProperty = 42;
+            obj.ObjectProperty = new ExpandoObject();
+            obj.ObjectProperty.AnotherProperty = "Yes, another property";
+
+            Assert.AreEqual("This is a string", obj.StringProperty);
+            Assert.AreEqual(42, obj.IntProperty);
+            Assert.IsNotNull(obj.ObjectProperty);
+            Assert.AreEqual("Yes, another property", obj.ObjectProperty.AnotherProperty);
         }
 
         [Test]
