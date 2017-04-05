b531f4e640ec813169a50d4f75adf79064f90d93
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-05 10:19:08 -0500

    implicitly typed variable

--- CSharp.Course.Work/Version3.cs
+++ CSharp.Course.Work/Version3.cs
@@ -13,7 +13,17 @@ namespace CSharp.Course.Work
         [Test]
         public void ImplicitlyTypedLocalVariables()
         {
-            Assert.Ignore();
+            // Assert.Ignore();
+
+            int int1 = 1;
+            var var1 = 1;
+
+            // var is not a variant but implicitly typed
+            // uncomplete to see compiler error
+            // var1 = 1d;
+
+            Assert.AreEqual(int1, var1);
+            Assert.AreEqual(int1.GetType(), var1.GetType());
         }
 
         [Test]
