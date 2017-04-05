817bbe40dda89312b5085a3007d8f410386ba233
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-05 10:53:13 -0500

    extension methods

--- CSharp.Course.Work/Version3.cs
+++ CSharp.Course.Work/Version3.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharp.Course.Work.VersionThree;
 
 namespace CSharp.Course.Work
 {
@@ -100,16 +101,19 @@ namespace CSharp.Course.Work
             Assert.AreEqual("Yes, another property", obj.ObjectProperty.AnotherProperty);
         }
 
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/dotnet/articles/csharp/programming-guide/classes-and-structs/extension-methods
+        /// </summary>
         [Test]
         public void ExtensionMethods()
         {
-            Assert.Ignore();
-        }
+            // Assert.Ignore();
 
-        [Test]
-        public void QueryExpressions()
-        {
-            Assert.Ignore();
+            var text = @"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+            var wordCount = text.WordCount();
+
+            Assert.AreEqual(91, wordCount);
         }
 
         [Test]
---
+++ CSharp.Course.Work/VersionThree/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Course.Work.VersionThree
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/articles/csharp/programming-guide/classes-and-structs/extension-methods
+    /// </summary>
+    public static class ExtensionMethods
+    {
+        public static int WordCount(this String text)
+        {
+            return text.Split(new char[] { ' ', '.', '?', ',' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
